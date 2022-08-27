ae30d479d92f6e6267f948fed6de9229b7abe1f1
JefferySCZ <jeffery.sim.cz@gmail.com>
2022-08-27 21:47:18 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo') // load todo model
 const app = express()
-
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -12,6 +12,8 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const db = mongoose.connection
 
@@ -27,9 +29,20 @@ db.once('open', () => {
 
 app.get('/', (req, res) => {
   Todo.find() // get all data from Todo Model
-  .lean() //  change mongoose's model to clean JS data array
-  .then(todos => res.render('index', { todos })) // data send to index template
-  .catch(error => console.error(error))
+    .lean() //  change mongoose's model to clean JS data array
+    .then((todos) => res.render('index', { todos })) // data send to index template
+    .catch((error) => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name //through req.body get data "name"
+  return Todo.create({ name }) //save to database
+    .then(() => res.redirect('/')) // after add new , back to home page
+    .catch((error) => console.log(error))
 })
 
 app.listen(3000, () => {
